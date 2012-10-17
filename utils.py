46a6960d8aaef58e555b53a01382b55adad47b5d
Noah Tilton <noaht@wolfram.com>
2012-10-17 09:42:58 -0500

    enabling notifications

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def notify(message):
     Notify.init("new mail")
     if message.is_multipart():  msg = "%s attachments ..." % len(message.get_payload())
     else:                       msg = "%s ..." % message.get_payload()[:100]
-    n = Notify.Notification.new("\n".join([message["from"], message["subject"]]),
+    n = Notify.Notification.new("\n".join([message.get("from", ""), message.get("subject", "")]),
                                 msg,
                                 "dialog-information")
     n.set_timeout(0)
@@ -18,8 +18,8 @@ def maildirname(maildir):
 
 
 def mv(src, dst, message, key):
-    print("mv %s/%s -> %s" % (maildirname(src), message["subject"], maildirname(dst)))
-    #notify(message)
+    print("mv %s/%s -> %s" % (maildirname(src), message.get("subject", ''), maildirname(dst)))
+    notify(message)
     dst.lock()
     dst.add(message)
     dst.flush()
